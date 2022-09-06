688bbc5c39c3d19e18d9692401894b0344cfa592
Radu Radu <neoradu@gmail.com>
2022-09-06 12:34:22 +0300

    stack overflow
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Created by jt on 6/13/17.
